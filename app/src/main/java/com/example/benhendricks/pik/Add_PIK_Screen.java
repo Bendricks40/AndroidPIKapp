c43e1c1129d0d6e337ad0e4152a613b8f02f073a
Ben Hendricks <ben.hendricks@cfacorp.com>
2018-01-31 21:24:08 -0500

    Initial commit

---
+++ app/src/main/java/com/example/benhendricks/pik/Add_PIK_Screen.java
@@ -0,0 +1,17 @@
+package com.example.benhendricks.pik;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class Add_PIK_Screen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_add__pik__screen);
+    }
+}
